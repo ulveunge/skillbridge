ed0e096d3687a63385bab8fb9a7765a20967d112
Emin Isaev <emin.isaev97@gmail.com>
2023-11-15 01:31:05 +0700

    fix notification and burger menu

--- src/ts/controllers/MobileNavController.ts
+++ src/ts/controllers/MobileNavController.ts
@@ -1,11 +1,24 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class MobileNavController extends Controller {
-  static targets = ["container"];
+  static targets = ["container", "icon"];
+
+  open = false;
 
   toggle() {
+    this.open = !this.open;
+    this.switchIcon();
     this.containerTarget.classList.toggle("open");
   }
 
+  switchIcon() {
+    const newIconIndex = this.open ? 1 : 0;
+    const prevIconIndex = this.open ? 0 : 1;
+
+    this.iconTarget.children[newIconIndex].classList.add("visible");
+    this.iconTarget.children[prevIconIndex].classList.remove("visible");
+  }
+
   declare readonly containerTarget: HTMLUListElement;
+  declare readonly iconTarget: SVGAElement;
 }
