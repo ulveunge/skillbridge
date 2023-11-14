db71566616a6e4a9af475c8968a6e3683657c4bd
Emin Isaev <emin.isaev97@gmail.com>
2023-11-15 03:56:01 +0700

    add routes for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import handlebars from "vite-plugin-handlebars";
 import { resolve } from "path";
 
+const routes = {
+  main: resolve(__dirname, "index.html"),
+  ui: resolve(__dirname, "ui.html"),
+};
+
 export default {
   plugins: [
     handlebars({
       partialDirectory: resolve(__dirname, "src/partials"),
     }),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        ...routes,
+      },
+    },
+  },
 };
