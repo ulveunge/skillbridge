424220812a928a6a9af3ee24a8cfb3d1a27e978b
Emin Isaev <emin.isaev97@gmail.com>
2023-12-02 20:00:18 +0700

    create footer

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import { resolve } from "path";
 
 const routes = {
   main: resolve(__dirname, "index.html"),
+  courses: resolve(__dirname, "courses.html"),
+  about: resolve(__dirname, "about.html"),
+  pricing: resolve(__dirname, "pricing.html"),
+  contact: resolve(__dirname, "contact.html"),
   ui: resolve(__dirname, "ui.html"),
+  "sign-in": resolve(__dirname, "sign-in.html"),
+  "sign-up": resolve(__dirname, "sign-up.html"),
 };
 
 export default {
