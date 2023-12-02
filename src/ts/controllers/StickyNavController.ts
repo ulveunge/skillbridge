d82d564561094f2d3fec1828bcadf5a42e4f3892
Emin Isaev <emin.isaev97@gmail.com>
2023-12-03 01:51:20 +0700

    small fix

--- src/ts/controllers/StickyNavController.ts
+++ src/ts/controllers/StickyNavController.ts
@@ -6,12 +6,13 @@ export default class StickyNavController extends Controller<HTMLDivElement> {
   sticked: boolean = false;
 
   connect() {
+    window.addEventListener("load", this.setAbsoluteHeight.bind(this));
     window.addEventListener("scroll", this.stickNav.bind(this));
     window.addEventListener("resize", this.setAbsoluteHeight.bind(this));
-    this.setAbsoluteHeight();
   }
 
   disconnect() {
+    window.removeEventListener("load", this.setAbsoluteHeight);
     window.removeEventListener("scroll", this.stickNav);
     window.removeEventListener("resize", this.setAbsoluteHeight);
   }
@@ -36,6 +37,8 @@ export default class StickyNavController extends Controller<HTMLDivElement> {
   }
 
   setAbsoluteHeight() {
+    console.log(this.absoluteHeight);
+
     this.element.style.removeProperty("--absolute-height");
 
     this.element.style.setProperty(
